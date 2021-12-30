64e461f38b3eb9184f165ad7e48924138d23fb08
Pham Tung Lam <tunglam.pham@lixil.com>
2021-12-30 20:06:32 +0700

    dohuyetap + taobaiviet

--- dohuyetap/js/shopping.js
+++ dohuyetap/js/shopping.js
@@ -1,4 +1,52 @@
 $(function () {
+  var form = $('#huyetap-form');
+  form.validate({
+    rules: {
+      name: 'required',
+      phone: {
+        required: true,
+        maxlength: 10,
+        minlength: 10,
+        digits: true,
+      },
+      confirmPhone: {
+        required: true,
+        maxlength: 10,
+        minlength: 10,
+        digits: true,
+        equalTo: '#phone-huyetap',
+      },
+      email: {
+        email: true,
+      },
+      address: {
+        required: true,
+      },
+    },
+    messages: {
+      name: 'Hãy nhập họ và tên của bạn',
+      address: 'Hãy nhập địa chỉ của bạn',
+      phone: {
+        required: 'Hãy nhập số điện thoại của bạn',
+        maxlength: 'Số điện thoại chỉ có 10 số',
+        minlength: 'Số điện thoại chỉ có 10 số',
+        digits: 'Hãy nhập sô 1-9',
+      },
+      confirmPhone: {
+        required: 'Hãy nhập số điện thoại của bạn',
+        maxlength: 'Số điện thoại chỉ có 10 số',
+        minlength: 'Số điện thoại chỉ có 10 số',
+        digits: 'Hãy nhập sô 1-9',
+        equalTo: 'Số điện thoại chưa trùng khớp',
+      },
+      email: {
+        email: 'Email chưa hợp lệ',
+      },
+    },
+  });
+});
+
+$(function () {
   var dataUrl =
     'https://script.google.com/macros/s/AKfycbwPu8pg27OnnmoNTB_35PvksuV_QwSXepws5M2DknSI9aoQ5rk/exec';
 
